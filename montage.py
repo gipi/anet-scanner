3d430a9d8a5a32fc2049e7e91361a24f467f3b5a
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2017-11-25 18:59:03 +0100

    montage: WTF

--- montage.py
+++ montage.py
@@ -35,29 +35,37 @@ def image_create(width, height):
 if __name__ == '__main__':
 	start = int(sys.argv[1]), int(sys.argv[2])
 	end   = int(sys.argv[3]), int(sys.argv[4])
+	deltaX = int(sys.argv[5])
+	deltaY = int(sys.argv[6])
 
-	size = 120
+	print locals()
+
+	size = 200
 
 	images = load_images(start, end, size=size)
 
 	columns = (end[0] - start[0] + 1)
 	rows    = (end[1] - start[1] + 1)
 	print 'creating canvas with shape (%d, %d)' % (columns, rows)
+	#canvas  = image_create(images[0][0].shape[1]*columns, images[0][0].shape[0]*rows)
 	canvas  = image_create(size*columns, size*rows)
 
-	offsetX = 0
+	# take the first image as standard size
+	shape = images[0][0].shape
 
-	for x in xrange(start[0], end[0] + 1):
-		offsetY = 0
-		images_row = images[x]
-		for y in xrange(start[1], end[1] + 1):
-			src = images_row[y]
-			print 'shape at %d %d: (%d, %d) with offset %d, %d' % (x, y, src.shape[0], src.shape[1], offsetX, offsetY)
-			canvas[canvas.shape[0] - offsetY - src.shape[0]:canvas.shape[0] - offsetY, offsetX:offsetX + src.shape[1]] = src
+	for idx_x in xrange(end[0], start[0] - 1, -1):
+		images_row = images[idx_x]
+		for idx_y in xrange(end[1], start[1] - 1, -1):
+			src = images_row[idx_y]
+			M = cv2.getRotationMatrix2D((0, 0), -4, 1)
+			src = cv2.warpAffine(src, M, (src.shape[1], src.shape[0]))
+			print 'shape at %d %d: (%d, %d)' % (idx_x, idx_y, shape[0], shape[1])
+			#import ipdb;ipdb.set_trace()
+			x = idx_x*(shape[1] + deltaX)
+			y = canvas.shape[0] - (idx_y + 1)*(shape[0]) + idx_y*deltaY
 
-			offsetY += src.shape[0]
+			canvas[y:y + shape[0], x:x + shape[1]] = src
 
-		offsetX += src.shape[1]
 
 	cv2.imshow('Editing window', canvas)
 	cv2.waitKey(0)
